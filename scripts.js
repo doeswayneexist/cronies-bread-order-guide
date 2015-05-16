ced5feb6290607e2e41342090e77cb9cf06a8cd7
Wayne Smith <doeswayneexist@gmail.com>
2015-05-16 02:24:11 -0700

    Added a test line.

---
+++ scripts.js
@@ -0,0 +1,92 @@
+//get current bread
+var currentBread;
+var extraPeople;
+
+//set the sandwich ratio. this is how many people out of 10 get sandwiches
+var sandwichRatio = 3;
+
+//set the backup supply. this is what portion of the daily usage we should add as backup. set at an extra 75%
+//var backUp = .75;
+var backUp = 48;
+//connect with the submit button
+var subBtn = document.getElementById("sub");
+subBtn.onclick = function () {
+    extraPeople = document.getElementById("extraPeople").value;
+    currentBread = document.getElementById('rolls').value;
+    calculateBread(currentBread);
+};
+
+//figure out what the estimatedUsage is
+var today = new Date();
+var weekday = today.getDay();
+
+//setting the values for the weekdays
+var sundayAverage = 35;
+var mondayAverage = 37;
+var tuesdayAverage = 27;
+var wednesdayAverage = 39;
+var thursdayAverage = 25;
+var fridayAverage = 48;
+var saturdayAverage = 46;
+
+
+function calculateBread(cB) {
+
+    //finding the estimated usage for the day and the bread par
+    //what is the breadpar? the next days usage, plus some extra
+    var estimatedUsage;
+    var breadPar;
+
+    if (weekday == 0) {
+        estimatedUsage = sundayAverage;
+        breadPar = mondayAverage + (mondayAverage * backUp);
+    } else if (weekday == 1) {
+        estimatedUsage = mondayAverage;
+    } else if (weekday == 2) {
+        estimatedUsage = tuesdayAverage;
+    } else if (weekday == 3) {
+        estimatedUsage = wednesdayAverage;
+        breadPar = thursdayAverage + (thursdayAverage * backUp);
+    } else if (weekday == 4) {
+        estimatedUsage = thursdayAverage;
+        //breadPar = fridayAverage + (fridayAverage * backUp);
+        breadPar = fridayAverage + backUp;
+    } else if (weekday == 5) {
+        estimatedUsage = fridayAverage;
+        //just gonna throw 3 dozen in for extra on the weekend
+        //this is temporary
+        breadPar = saturdayAverage + sundayAverage + backUp;
+    } else if (weekday == 6) {
+        estimatedUsage = saturdayAverage;
+    }
+
+    //determine what you should have left at the end of the day
+    var projectedLeftovers = cB - estimatedUsage;
+
+    //dont let projected leftovers be negative
+    if (projectedLeftovers < 0) {
+        projectedLeftovers = 0;
+    }
+
+    //add any extra parties for tomorrow
+    if (extraPeople > 0) {
+        breadPar += parseInt(extraPeople / sandwichRatio, 10);
+    }
+
+    //get the bread order
+    var breadOrder = breadPar - projectedLeftovers;
+
+    //dont let bread order be negative
+    if (breadOrder < 0) {
+        breadOrder = 0;
+    }
+
+    //convert to dozen
+    var asDozen = parseInt(breadOrder / 12, 10);
+    if (breadOrder % 12 >= 6) {
+        asDozen++;
+    }
+
+    //spit out a reading
+    alert("Order " + asDozen + " dozen rolls. ");
+}
